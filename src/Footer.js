c73b7057625f4f4d5a1cfb8ca17daca3e5e13eb8
parnexcodes <parnexcodes@gmail.com>
2022-10-16 15:18:53 +0530

    add initial files

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
       <div className="py-2 text-center">
         <footer
-          className="text-white sticky top-[100vh] pt-11 pb-5 mt-14"
+          className="text-white sticky top-[100vh] pt-4 pb-4 mt-14"
           style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
         >
           <p>
--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ const Header = () => {
     return (
         <div className="flex mx-8 justify-center">
             <li className="list-none my-8 pr-2">
-                <a className="rounded-md px-4 py-2 pt-1 bg-zinc-800 text-gray-300 " href="https://apod.nasa.gov/apod/archivepix.html">Discover the <span className="text-red-500">Cos</span> <span className="text-blue-600">Mos</span></a>
+                <a className="rounded-md px-4 py-2 pt-1 bg-zinc-800 text-gray-300 " href="https://apod.nasa.gov/apod/archivepix.html" target="_blank">Discover the <span className="text-red-500">Cos</span> <span className="text-blue-600">Mos</span></a>
             </li>
         </div>
     );
